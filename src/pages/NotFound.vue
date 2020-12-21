a9f45f35e45b049d5bf1ae95c891cdc4a9a2916e
stefan dili <stefan.dili.021@gmail.com>
2020-12-21 17:33:49 +0100

    made page not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page Not Found</h2>
+      <p>
+        Go back to our
+        <router-link to="/coaches">
+          coaches index
+        </router-link>
+      </p>
+    </base-card>
+  </section>
+</template>
